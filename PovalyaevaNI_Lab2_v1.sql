d691f0d980fd690b706067d757e3cb8a3a0785e4
Nadya Povalyaeva <iriejamaica@outlook.com>
2019-10-20 16:39:50 +0300

    Update labwork #2 (special for connection with task 3.1)

--- PovalyaevaNI_Lab2_v1.sql
+++ PovalyaevaNI_Lab2_v1.sql
@@ -37,7 +37,7 @@ GO
 -- не включая индексы, ограничения и триггеры:
 
 CREATE TABLE [dbo].[Address](
-	-- [AddressID] [int] IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
+	[AddressID] [int] IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
 	[AddressLine1] [nvarchar](60) NOT NULL,
 	[AddressLine2] [nvarchar](60) NULL,
 	[City] [nvarchar](30) NOT NULL,
@@ -66,14 +66,15 @@ ALTER TABLE [dbo].[Address]
 GO
 
 -- Заполните новую таблицу данными из Person.Address. 
--- Выберите для вставки только те адреса, где значение поля CountryRegionCode = СUSТ из таблицы StateProvince. 
+-- Выберите для вставки только те адреса, где значение поля CountryRegionCode = US из таблицы StateProvince. 
 -- Также исключите данные, где PostalCode содержит буквы. 
 -- Для группы данных из полей StateProvinceID и PostalCode выберите только строки с максимальным AddressID 
 -- (это можно осуществить с помощью оконных функций):
 
+SET IDENTITY_INSERT [dbo].[Address] ON
 INSERT INTO [dbo].[Address] 
-	([AddressLine1], [AddressLine2], [City], [StateProvinceID], [PostalCode], [ModifiedDate])
-	(SELECT [a].[AddressLine1], [a].[AddressLine2], [a].[City], [a].[StateProvinceID], [a].[PostalCode], [a].[ModifiedDate]
+	([AddressID], [AddressLine1], [AddressLine2], [City], [StateProvinceID], [PostalCode], [ModifiedDate])
+	(SELECT [a].[AddressID], [a].[AddressLine1], [a].[AddressLine2], [a].[City], [a].[StateProvinceID], [a].[PostalCode], [a].[ModifiedDate]
 	FROM (
 		SELECT TOP(1) WITH TIES *
 		FROM [Person].[Address]
@@ -83,6 +84,7 @@ INSERT INTO [dbo].[Address]
 	ON [a].[StateProvinceID] = [st].[StateProvinceID] 
 		AND [st].[CountryRegionCode] = 'US'
 		AND [a].[PostalCode] NOT LIKE '%[A-Za-z]%')
+SET IDENTITY_INSERT [dbo].[Address] OFF 
 
 -- Уменьшите размер поля City на NVARCHAR(20):
 
